7a43f12c0e15670a1837097b1d7b5be34447959f
ChiWoo-Shin <shin8037@naver.com>
2023-04-24 23:11:28 +0900

    노마드코더 1.4장

---
+++ components/NavBar.js
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+import styles from "./NavBar.module.css"
+
+export default function NavBar() {
+    const router = useRouter();
+    
+  return (
+    <nav >
+        {/* <Link legacyBehavior href="/">
+            <a style={{color: 'red'}}>Home</a>
+        </Link> */}
+        <Link className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`} href="/">
+            Home
+        </Link>
+        <Link className={[
+            styles.link,
+            router.pathname === "/about" ? styles.active : "",
+            ].join(" ")} href="/about">
+            About
+        </Link>
+    </nav>
+  )
+}
