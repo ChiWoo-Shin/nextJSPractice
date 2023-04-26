3079f8b58a7d5afb6e568d664f8942984a6800f8
ChiWoo-Shin <shin8037@naver.com>
2023-04-26 23:28:33 +0900

    2.2장 redirect, rewrite까지 끝

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
-import NavBar from "@/components/NavBar";
+import Head from "next/head";
+import HeadTitle from "@/components/headTitle";
 
 export default function About() {
     return (
         <div>
+            <HeadTitle title="About" />
             <h1>About</h1>
         </div>
     )
